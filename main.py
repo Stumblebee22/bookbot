f94706cfc4dc03ce2363d31f60342546e8f58340
Stumblebee <yorian222kip@gmail.com>
2025-02-22 22:34:28 +0100

    modified main.py and stats.py, attempted to modify .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_word_count
 from stats import get_character_count
+from stats import sorted_list
+
 
 def main():
     book_path = "books/frankenstein.txt"
@@ -7,8 +9,22 @@ def main():
     num_words = get_word_count(text)
     character_count = get_character_count(text)
     message = f"{num_words} words found in the document"
+    neat_sorted_list = sorted_list(character_count)
+    character_report = ""
+    for char_dict in neat_sorted_list:
+        char = list(char_dict.keys())[0]
+        count = list(char_dict.values())[0]
+        if char.isalpha(): 
+            character_report += f"{char}: {count}\n"
+        
     
-    print(character_count)
+    report = f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------
+{character_report}============= END ==============="""
+    print(report)
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -12,3 +12,13 @@ def get_character_count(text):
         if character in character_counter:
             character_counter[character] +=1 
     return character_counter
+
+def sorted_list(character_counter):
+    list_from_dict = [{key: value} for key, value in character_counter.items()]
+    def sort_on(dict):
+        return list(dict.values())[0]
+
+    list_from_dict.sort(reverse=True, key=sort_on)
+    return(list_from_dict)
+
+
